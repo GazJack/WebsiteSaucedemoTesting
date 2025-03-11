8ff1fd95abe5c36934b80211e1a02ccbf6d73dc7
GazJack <greta.jackaite@gmail.com>
2025-03-11 23:08:49 +0200

    2: 5

---
+++ cypress/e2e/tests/5_logout.cy.js
@@ -0,0 +1,25 @@
+const users = [
+    'standard_user',
+    'problem_user',
+    'performance_glitch_user',
+    'error_user',
+    'visual_user'
+];
+
+describe('Test Case 4', () => {
+    users.forEach(user => {
+        describe(`Logout with user: ${user}`, () => {
+            beforeEach(() => {
+                cy.login(user, 'secret_sauce');
+
+            });
+
+            it('Logout', () => {
+               cy.get("#react-burger-menu-btn").click();
+               cy.get('[data-test="logout-sidebar-link"]').click();
+               cy.get('[data-test="login-container"]').should('be.visible');
+            });
+        });
+    });
+});
+
